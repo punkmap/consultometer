51e55e4eb98c85bc56d116583855ba86b4b89b22
Tyler McCracken <tylerdean.w@gmail.com>
2020-02-25 18:00:24 -0500

    set localhost to correct port

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const prod = {
     API_URL: 'https://consultometer.api.punkmap.com',
 };
 const dev = {
-    API_URL: 'http://localhost:3000'
+    API_URL: 'http://localhost:5000'
 };
 console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
