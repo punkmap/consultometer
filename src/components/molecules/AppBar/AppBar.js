25534f40ecdbc9b7575f55ae04f049a9cf9d8f96
Tyler McCracken <tylerdean.w@gmail.com>
2020-03-12 13:59:27 -0400

    pipeline to test site 2

--- src/components/molecules/AppBar/AppBar.js
+++ src/components/molecules/AppBar/AppBar.js
@@ -195,7 +195,7 @@ class ButtonAppBar extends Component {
           <PublishIcon/>
         </IconButton>
     }
-    const dialogTitle = 'login t'
+    const dialogTitle = 'login test'
     const dialogContent = <form>
             <TextField
                 autoFocus
