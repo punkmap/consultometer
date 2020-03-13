85181a8658409ed4f9e0023fa8fb835f63d3f75f
Tyler McCracken <tylerdean.w@gmail.com>
2020-03-12 21:40:12 -0400

    meeting detail phone debugging 11

--- src/components/molecules/MeetingDetail/MeetingDetail.js
+++ src/components/molecules/MeetingDetail/MeetingDetail.js
@@ -219,19 +219,160 @@ class MeetingDetail extends Component {
     this.setState({switchState: !this.state.switchState});
   };
   render() {
+
+    const { classes } = this.props;
     return (
       <DeviceOrientation lockOrientation={'landscape'}>
         {/* Will only be in DOM in landscape */}
         <Orientation orientation='landscape' alwaysRender={false}>
-          <div>
-            <p>Only visible in landscape</p>
-          </div>
+          <Grid 
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            style={{ minHeight: '80vh' }}>
+            <Grid item xs={9} md={9} lg={9}>
+              
+              <Grid container direction="row" justify="space-between">
+                <Grid item>
+                  <Typography variant="h4" gutterBottom>{this.state.detailItem && this.state.detailItem.title}</Typography>
+                </Grid>
+                <Grid item>
+                  <IconBtn 
+                        icon={<CloseIcon fontSize="large"/>} 
+                        click={() => this.cancel()}
+                      />
+                </Grid>
+              </Grid>
+              <Grid item >
+                  <Grid container direction="row" justify="space-evenly">
+                    <Grid item xs={3}>
+                        <Typography variant="h5">Total</Typography>
+                        <Typography variant="h2" className={classes.timer}> {msTime.msToHMS(this.state.time)}</Typography>
+                        <Typography variant="h2" className={classes.cost}>${msTime.msToCost(this.state.meeting.rate, this.state.time)}</Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Typography variant="h5">A/V</Typography>
+                        <Typography variant="h2" className={classes.timer}> {msTime.msToHMS(this.state.avTime)}</Typography>
+                        <Typography variant="h2" className={classes.cost}>${msTime.msToCost(this.state.meeting.rate, this.state.avTime)}</Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid container direction="row" justify="space-evenly">
+                    <Grid item>
+                      <Box display={ this.state.meeting.infoOnly ? "none" : "block" }>
+                        <IconBtn 
+                          icon={<PlayArrowIcon fontSize="large"/>} 
+                          click={(event) => this.startMeeting(this.state.meeting)}
+                          active={this.state.timerRunning}
+                        />
+                        <IconBtn 
+                          icon={<PauseIcon fontSize="large"/>} 
+                          click={(event) => this.pauseMeeting(this.state.meeting)}
+                          active={this.state.timerPaused}
+                        />
+                        <IconBtn 
+                          icon={<StopIcon fontSize="large"/>} 
+                          click={(event) => this.stopMeeting(this.state.meeting)}
+                        />
+                        <IconBtn 
+                          icon={<RefreshIcon fontSize="large"/>} 
+                          click={(event) => this.props.refreshMeeting(event, this.state.meeting)}
+                        />
+                        <FormControlLabel 
+                          control={
+                          <Switch size="medium" 
+                                  checked={this.state.switchState} 
+                                  onChange={this.toggleSwitchState.bind(this)} 
+                          />} 
+                          label="A/V" 
+                        />
+                      </Box>
+                    </Grid>
+                  </Grid>          
+              </Grid>
+            </Grid>
+          </Grid>;
         </Orientation>
         {/* Will stay in DOM, but is only visible in portrait */}
-        <Orientation orientation='portrait'>
-          <div>
-            <p>Please rotate your device</p>
-          </div>
+        <Orientation orientation='portrait' alwaysRender={false}>
+          <Grid 
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            style={{ minHeight: '80vh' }}>
+            <Grid item xs={10} md={6} lg={6}>
+            <Typography variant="h4" gutterBottom>{this.state.detailItem && this.state.detailItem.title}</Typography>
+            <Typography variant="h5" gutterBottom>{this.state.detailItem && this.state.detailItem.purpose}</Typography>
+            {this.state.detailItem && this.state.detailItem.attendees && this.state.detailItem.attendees.map(data => {
+              let icon;
+
+              return (
+                <Chip
+                  key={data.key}
+                  icon={icon}
+                  label={data.value.name}
+                  className={classes.chip}
+                />
+              );
+            })}
+            <Grid item >
+                <Grid container direction="row" justify="space-evenly">
+                  <Grid item xs={3}>
+                      <Typography variant="body2">Total</Typography>
+                      <Typography variant="body2" className={classes.timer}> {msTime.msToHMS(this.state.time)}</Typography>
+                      <Typography variant="body2" className={classes.cost}>${msTime.msToCost(this.state.meeting.rate, this.state.time)}</Typography>
+                  </Grid>
+                  <Grid item xs={3}>
+                      <Typography variant="body2">A/V</Typography>
+                      <Typography variant="body2" className={classes.timer}> {msTime.msToHMS(this.state.avTime)}</Typography>
+                      <Typography variant="body2" className={classes.cost}>${msTime.msToCost(this.state.meeting.rate, this.state.avTime)}</Typography>
+                  </Grid>
+                  
+                </Grid>
+                <Grid container direction="row" justify="space-between">
+                  <Grid item>
+                    <Box display={ this.state.meeting.infoOnly ? "none" : "block" }>
+                      <IconBtn 
+                        icon={<PlayArrowIcon fontSize="small"/>} 
+                        click={(event) => this.startMeeting(this.state.meeting)}
+                        active={this.state.timerRunning}
+                      />
+                      <IconBtn 
+                        icon={<PauseIcon fontSize="small"/>} 
+                        click={(event) => this.pauseMeeting(this.state.meeting)}
+                        active={this.state.timerPaused}
+                      />
+                      <IconBtn 
+                        icon={<StopIcon fontSize="small"/>} 
+                        click={(event) => this.stopMeeting(this.state.meeting)}
+                      />
+                      <IconBtn 
+                        icon={<RefreshIcon fontSize="small"/>} 
+                        click={(event) => this.props.refreshMeeting(event, this.state.meeting)}
+                      />
+                      <FormControlLabel 
+                        //className={classes.switchControl}
+                        control={
+                        <Switch size="small" 
+                                checked={this.state.switchState} 
+                                onChange={this.toggleSwitchState.bind(this)} 
+                        />} 
+                        label="A/V" 
+                      />
+                    </Box>
+                  </Grid>
+                </Grid>          
+            </Grid>
+              <Grid container className={classes.buttonBar}>
+                <Grid item className={classes.actionGrid}>
+                  <Button variant="contained" color="primary" onClick={this.cancel.bind(this)}>
+                    close
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>;
         </Orientation>
       </DeviceOrientation>
     )
